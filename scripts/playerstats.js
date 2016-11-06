af80da1bfec8473bc3c87c0ca41ba0f944c62e32
James C <3661681+JamesMCo@users.noreply.github.com>
2016-11-06 23:47:57 +0000

    Added basics of player class stats

--- scripts/playerstats.js
+++ scripts/playerstats.js
@@ -4,36 +4,47 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.wynncraft.com/public_api.php?action=playerStats&command=" + playername);
 xhr.send();
 
-xhr.addEventListener("readystatechange", processRequest, false)
+xhr.addEventListener("readystatechange", processRequest, false);
 
 function processRequest(e) {
   if (xhr.readyState == 4 && xhr.status == 200) {
     var response = JSON.parse(xhr.responseText);
+
     if (response.error == "Player not found") {
-      document.getElementById("namerank").innerHTML   = "Player not found!"
-      document.getElementById("stats").innerHTML      = ""
+      document.getElementById("namerank").innerHTML   = "Player not found!";
+      document.getElementById("stats").innerHTML      = "";
       return
     }
-    document.getElementById("username").innerHTML     = response.username
+
+    document.getElementById("username").innerHTML     = response.username;
+
     if (response.tag != "") {
-      document.getElementById("rank").innerHTML       = response.rank + " (" + response.tag + ")"
+      document.getElementById("rank").innerHTML       = response.rank + " (" + response.tag + ")";
     }
     else {
-      document.getElementById("rank").innerHTML       = response.rank
+      document.getElementById("rank").innerHTML       = response.rank;
     }
+
     if (response.current_server != "null") {
-      document.getElementById("c_server").innerHTML   = "Currently Online: " + response.current_server
+      document.getElementById("c_server").innerHTML   = "Currently Online: " + response.current_server;
     }
-    document.getElementById("playtime").innerHTML     = response.playtime + " minutes<br>≈ " + Math.round((response.playtime / 60) * 100) / 100 + " hours"
-    document.getElementById("first_join").innerHTML   = response.first_join_friendly
-    document.getElementById("last_join").innerHTML    = response.last_join_friendly
-    document.getElementById("items_ided").innerHTML   = response.global.items_identified
-    document.getElementById("mobs_killed").innerHTML  = response.global.mobs_killed
-    document.getElementById("pvp_kills").innerHTML    = response.global.pvp_kills
-    document.getElementById("pvp_deaths").innerHTML   = response.global.pvp_deaths
-    document.getElementById("chests_found").innerHTML = response.global.chests_found
-    document.getElementById("logins").innerHTML       = response.global.logins
-    document.getElementById("deaths").innerHTML       = response.global.deaths
-    document.getElementById("total_level").innerHTML  = response.global.total_level
+
+    document.getElementById("playtime").innerHTML     = response.playtime + " minutes<br>≈ " + Math.round((response.playtime / 60) * 100) / 100 + " hours";
+    document.getElementById("first_join").innerHTML   = response.first_join_friendly;
+    document.getElementById("last_join").innerHTML    = response.last_join_friendly;
+    document.getElementById("items_ided").innerHTML   = response.global.items_identified;
+    document.getElementById("mobs_killed").innerHTML  = response.global.mobs_killed;
+    document.getElementById("pvp_kills").innerHTML    = response.global.pvp_kills;
+    document.getElementById("pvp_deaths").innerHTML   = response.global.pvp_deaths;
+    document.getElementById("chests_found").innerHTML = response.global.chests_found;
+    document.getElementById("logins").innerHTML       = response.global.logins;
+    document.getElementById("deaths").innerHTML       = response.global.deaths;
+    document.getElementById("total_level").innerHTML  = response.global.total_level;
+
+    for (var c in response.classes) {
+      if (response.classes.hasOwnProperty(c)) {
+        document.getElementById("classdiv").innerHTML += "<div id='" + c + "div'><h2 class='titlecase'>" + c + "</h2><table id='" + c + "stats'><tr><th>Level</th><td>" + eval("response.classes." + c).level + "</td></tr></div>"
+      }
+    }
   }
 }
