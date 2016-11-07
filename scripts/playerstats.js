cf93359b57aa45720f6b0305e723e56ffb0e4e32
James C <3661681+JamesMCo@users.noreply.github.com>
2016-11-07 23:21:06 +0000

    Added current username to page title

--- scripts/playerstats.js
+++ scripts/playerstats.js
@@ -15,30 +15,31 @@ function processRequest(e) {
       return
     }
 
-    document.getElementById("username").innerHTML     = response.username;
+    document.getElementsByTagName("title")[0].innerHTML = response.username + " - WynnStats";
+    document.getElementById("username").innerHTML = response.username;
 
     if (response.tag != "") {
-      document.getElementById("rank").innerHTML       = response.rank + " (" + response.tag + ")";
+      document.getElementById("rank").innerHTML = response.rank + " (" + response.tag + ")";
     }
     else {
-      document.getElementById("rank").innerHTML       = response.rank;
+      document.getElementById("rank").innerHTML = response.rank;
     }
 
     if (response.current_server != "null") {
-      document.getElementById("c_server").innerHTML   = "Currently Online: " + response.current_server;
+      document.getElementById("c_server").innerHTML = "Currently Online: " + response.current_server;
     }
 
-    document.getElementById("playtime").innerHTML     = response.playtime + " minutes<br>≈ " + Math.round((response.playtime / 60) * 100) / 100 + " hours";
-    document.getElementById("first_join").innerHTML   = response.first_join_friendly;
-    document.getElementById("last_join").innerHTML    = response.last_join_friendly;
-    document.getElementById("items_ided").innerHTML   = response.global.items_identified;
-    document.getElementById("mobs_killed").innerHTML  = response.global.mobs_killed;
-    document.getElementById("pvp_kills").innerHTML    = response.global.pvp_kills;
-    document.getElementById("pvp_deaths").innerHTML   = response.global.pvp_deaths;
+    document.getElementById("playtime").innerHTML = response.playtime + " minutes<br>≈ " + Math.round((response.playtime / 60) * 100) / 100 + " hours";
+    document.getElementById("first_join").innerHTML = response.first_join_friendly;
+    document.getElementById("last_join").innerHTML = response.last_join_friendly;
+    document.getElementById("items_ided").innerHTML = response.global.items_identified;
+    document.getElementById("mobs_killed").innerHTML = response.global.mobs_killed;
+    document.getElementById("pvp_kills").innerHTML = response.global.pvp_kills;
+    document.getElementById("pvp_deaths").innerHTML = response.global.pvp_deaths;
     document.getElementById("chests_found").innerHTML = response.global.chests_found;
-    document.getElementById("logins").innerHTML       = response.global.logins;
-    document.getElementById("deaths").innerHTML       = response.global.deaths;
-    document.getElementById("total_level").innerHTML  = response.global.total_level;
+    document.getElementById("logins").innerHTML = response.global.logins;
+    document.getElementById("deaths").innerHTML = response.global.deaths;
+    document.getElementById("total_level").innerHTML = response.global.total_level;
 
     for (var classname in response.classes) {
       if (response.classes.hasOwnProperty(classname)) {
