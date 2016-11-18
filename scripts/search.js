60e8b8f987adef7f31ca8a62706687007f7ea240
James C <3661681+JamesMCo@users.noreply.github.com>
2016-11-18 21:03:08 +0000

    Fixed search box not getting "invalid" class

--- scripts/search.js
+++ scripts/search.js
@@ -1,14 +1,14 @@
 function gotoStatsURL() {
   if ($("#searchbox")[0].value !== "") {
     if ($("#radp")[0].checked == true) {
-      location.href = "./player.html?name=" + $("#searchbox")[0].value;
+      location.href = "./player?name=" + $("#searchbox")[0].value;
     }
     else {
-      location.href = "./guild.html?name=" + $("#searchbox")[0].value;
+      location.href = "./guild?name=" + $("#searchbox")[0].value;
     }
   }
   else {
-    $("searchbox").focus();
-    $("searchbox").addClass("invalid");
+    $("#searchbox").focus();
+    $("#searchbox").addClass("invalid");
   }
 }
